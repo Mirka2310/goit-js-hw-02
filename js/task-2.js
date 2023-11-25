1974f7c4da6e77022244d4ffc16d736ebaeb540f
Mirka2310 <gordinskamiroslava@gmail.com>
2023-11-25 10:33:41 +0100

    js

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+let check;
+function formatMessage(message, maxLength) {
+  if (message <= maxLength) {
+    check = message;
+  } else if (message > maxLength) {
+    check = message.slice(0, maxLength) + "...";
+  } else {
+    message = maxLength;
+  }
+  return check;
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
